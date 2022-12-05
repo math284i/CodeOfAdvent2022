ce73f3dcd38704d37dc7ddb44fa17add7feec4eb
Dyberg <mathias.rerup-dyberg@hotmail.com>
2022-12-05 12:58:35 -0600

    Clean up part2 a bit

--- December5/Program.cs
+++ December5/Program.cs
@@ -12,7 +12,7 @@ namespace December5
         {
             var fileName = "input.txt";
             var path = Path.Combine(Environment.CurrentDirectory, fileName);
-            //Part1(path);
+            //Part1(path, true);
             Part2(path);
         }
         
@@ -23,7 +23,7 @@ namespace December5
             return new string(charArray);
         }
 
-        private static void Part1(string path)
+        private static void Part1(string path, bool shouldReverse)
         {
             var lines = new List<string>();
             var strings = new List<string>();
@@ -66,7 +66,8 @@ namespace December5
                     var from = int.Parse("" + numbers[3]) - 1;
                     var to = int.Parse("" + numbers[5]) - 1;
                     var original = strings[from];
-                    var diff = Reverse(original?[^amount..original.Length]);
+                    var diff = shouldReverse ? Reverse(original?[^amount..original.Length]) 
+                        : original?[^amount..original.Length];
                     var tmp = strings[to];
                     var newOld = original?[0..^amount];
                     strings[to] = tmp + diff;
@@ -80,57 +81,7 @@ namespace December5
 
         private static void Part2(string path)
         {
-            var lines = new List<string>();
-            var strings = new List<string>();
-            var height = 7; //7
-            var width = 9 - 1; //9
-            foreach (var line in File.ReadLines(path))
-            {
-                if (lines.Count < height)
-                {
-                    lines.Add(line);
-                    strings.Add("");
-                }
-                else if (lines.Count == height)
-                {
-                    lines.Add(line);
-                    strings.Add("");
-                    strings.Add("");
-                    lines.Reverse();
-                    for (var i = 0; i < lines.Count; i++)
-                    {
-                        var index = 0;
-                        for (var j = 0; j < lines[i].Length; j++)
-                        {
-                            if (j != 1 && (j % 4 != 1)) continue;
-                            if (lines[i][j] != ' ')
-                            {
-                                var tmp = strings[index];
-                                strings[index] = tmp + lines[i][j];
-                            }
-                            index++;
-                            if (index > width) index = 0;
-                        }
-                    }
-                } 
-                else
-                {
-                    var numbers = line.Split(' ');
-                    if (numbers.Length != 6) continue;
-                    var amount = int.Parse("" + numbers[1]);
-                    var from = int.Parse("" + numbers[3]) - 1;
-                    var to = int.Parse("" + numbers[5]) - 1;
-                    var original = strings[from];
-                    var diff = original?[^amount..original.Length];
-                    var tmp = strings[to];
-                    var newOld = original?[0..^amount];
-                    strings[to] = tmp + diff;
-                    strings[from] = newOld;
-                }
-            }
-
-            var outputString = strings.Aggregate("", (current, str) => current + str[^1]);
-            Console.WriteLine(outputString);
+            Part1(path, false);
         }
     }
 }
